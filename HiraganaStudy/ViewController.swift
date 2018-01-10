10f636d94a17f992a278849772656ac0f43c6edf
Dominic Pretto <domenico@dominics-macbook-pro.local>
2018-01-10 10:00:32 +0900

    Initial Commit

---
+++ HiraganaStudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HiraganaStudy
+//
+//  Created by Dominic Pretto on 2018/01/10.
+//  Copyright © 2018 Dominic Pretto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
